66790aed6cadcc59f1823020077685308f796e94
Ava <1942728887@qq.com>
2021-02-05 14:44:31 +0800

    Site updated: 2021-02-05 14:44:29

---
+++ js/text.js
@@ -0,0 +1,25 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+  $("body").click(function(e) {
+    var a = new Array("从", "前", "有", "座", "山", "山", "里", "有", "座", "庙", "庙", "里", "有", "个", "老", "和", "尚", "老", "和", "尚", "对", "小", "和", "尚", "说");
+    var $i = $("<span>").text(a[a_idx]);
+    var x = e.pageX,
+    y = e.pageY;
+    $i.css({
+      "z-index": 99999,
+      "top": y - 28,
+      "left": x - a[a_idx].length * 8,
+      "position": "absolute",
+      "color": "#ff7a45"
+    });
+    $("body").append($i);
+    $i.animate({
+      "top": y - 180,
+      "opacity": 0
+    }, 1500, function() {
+      $i.remove();
+    });
+    a_idx = (a_idx + 1) % a.length;
+  });
+});
+</span>
